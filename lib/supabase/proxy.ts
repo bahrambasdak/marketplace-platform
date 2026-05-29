5c73afe99ff49829eeb1bbe8444846cde34dd13b
bahram <bahrambasdak@gmail.com>
2026-05-29 22:22:45 +0330

    noting

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -5,6 +5,7 @@ export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
+  
 
   // With Fluid compute, don't put this client in a global environment
   // variable. Always create a new one on each request.
