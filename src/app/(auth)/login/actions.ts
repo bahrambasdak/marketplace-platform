8dc8107a8beeb67efbcd4c7ec26852d8a52fc49d
bahram <bahrambasdak@gmail.com>
2026-07-05 17:55:39 +0330

    change folder structure

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/src/lib/supabase/server";
 
 const authSchema = z.object({
   email: z.email(),
